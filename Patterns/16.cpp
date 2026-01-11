13c6e2ab60cb89e9d376fd2e679ba87f0cd0b909
Tanishka <tanishkayadav6000@gmail.com>
2026-01-11 14:58:34 +0530

    update

--- Patterns/16.cpp
+++ Patterns/16.cpp
@@ -1,12 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n = 5;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-           cout<<i+j-1;
+            cout << i + j - 1;
         }
         cout << endl;
     }
